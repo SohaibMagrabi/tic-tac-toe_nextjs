ca0e928d015ce9b006fbab9015ecc262e905fc3b
[SohaibMagrabi] <[Sohaibmagrabi@gmail.com]>
2023-12-13 10:15:53 +0300

    the end

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -4,16 +4,39 @@ type CellProps = {
     id:number;
      go:string;
      setGo:Dispatch<SetStateAction<string>>
-     cells:string[];
-     setCells:Dispatch<SetStateAction<string[]>>;
-}
- const Cell = ({go, setGo, id,cells, setCells}:CellProps) => {
-    const handleClick = (e)=>{
-        console.log(id)
+    cells: string[];
+    setCells: Dispatch<SetStateAction<string[]>>;
+    cell: string;
+    winMsg:string 
 
+}
+const Cell = ({ go, setGo, id, cells, setCells,cell ,winMsg}: CellProps) => {
+    const handleClick = (e) => {
+        if(winMsg){
+            return ;
+        }
+        if (!cells[id]) {
+            if (go == "circle") {
+                handleCellChange("circle");
+                setGo("cross");
+            } else if (go == "cross") {
+                handleCellChange("cross");
+                setGo("circle");
+            }
+        }
     }
+    const handleCellChange = (cellTochange: string) => {
+        let copyCells = [...cells];
+        copyCells[id] = cellTochange;
+        setCells(copyCells);
+    }
+
+
+
     return (
-        <div className="square" onClick = {handleClick} ></div>
+        <div className="square" onClick = {handleClick} >
+            <div className={cell}>{cell ?(cell=='circle'?'O' :'X'):'' }</div>
+        </div>
 
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,60 @@
 "use client"
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import Cell from './components/cell'
-
+const winCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 export default function Home() {
-  const [cells, setCells] = useState(["","","","","","","","",""])
-  const[go, setGo] = useState("circle");
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
+  const [go, setGo] = useState("circle");
+  const [winMsg, setWinMsg] = useState("");
 
+  useEffect(() => {
+    winCombo.forEach((e) => {
+      const circleWin = e.every((e2) => cells[e2] === "circle")
+      const crossWin = e.every((e2) => cells[e2] === "cross")
+      if (circleWin) {
+        setWinMsg('circle win!')
+
+      } else if (crossWin) {
+        setWinMsg('cross win!')
+
+      }
+    })
+  },[cells,winMsg])/* useEffect depend on cells and winMsg if not depend it works OK */
+ useEffect(()=>{
+ if(cells.every((e)=>  e !== '') && !winMsg){
+  setWinMsg('draw')
+ }
+ })
   return (
-   <div  className="container" >
-     <div className="gameboard">
-     {cells.map((e,i) => (
-      <Cell id ={i} go={go} setGo={setGo} key={i} cells={cells} setCells={setCells}/>
-     ))}
-           
-     </div>
-    
-   </div >
+    <div className="container" >
+      <div className="gameboard">
+        {cells.map((e, i) => (
+          <Cell
+            cell={e}
+            id={i}
+            go={go}
+            setGo={setGo}
+            key={i}
+            cells={cells}
+            setCells={setCells}
+            winMsg={winMsg}          
+
+          />
+        ))}
+
+      </div>
+      <div>{winMsg}</div>
+      {!winMsg && <div>{`it is now ${go} turn`}</div>}
+    </div >
   );
 }
